666723115ae598b1eb45cd7933e31fc29d34bc54
alisev <45853990+alisev@users.noreply.github.com>
2023-10-28 19:00:18 +0300

    Update toCamelCase.js

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,4 +1,4 @@
-// Convert string to camel case
+// Convert string to camel case. E.g. the-stealth-warrior to theStealthWarrior
 function toCamelCase(str){
   const delimiter = /[-_]/;
   const arr = str.split(delimiter);
@@ -8,3 +8,16 @@ function toCamelCase(str){
   }
   return res;
 }
+
+function toCamelCaseV2(str) {
+  const capitalize = (substr) => {
+    return substr.charAt(1).toUpperCase();
+  };
+  return str.charAt(0) = str.slice(1).replace(/[-_][a-z]/g, (substr) => capitalize(substr));
+}
+
+// capitalize every word in sentence
+String.prototype.toJadenCase = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1).replace(/ [a-z]/g, (substr) => substr.toUpperCase());
+};
+
