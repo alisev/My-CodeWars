d331b61c7dcbeb29132e6a1037125242312c0e17
alisev <45853990+alisev@users.noreply.github.com>
2023-10-28 19:31:34 +0300

    Update toCamelCase.js

--- toCamelCase.js
+++ toCamelCase.js
@@ -1,4 +1,6 @@
-// Convert string to camel case. E.g. the-stealth-warrior to theStealthWarrior
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+// The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+// The next words should be always capitalized.
 function toCamelCase(str){
   const delimiter = /[-_]/;
   const arr = str.split(delimiter);
@@ -16,7 +18,9 @@ function toCamelCaseV2(str) {
   return str.charAt(0) = str.slice(1).replace(/[-_][a-z]/g, (substr) => capitalize(substr));
 }
 
-// capitalize every word in sentence
+// When writing on Twitter, [Jaden Smith] is known for almost always capitalizing every word.
+// For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+// Your task is to convert strings to how they would be written by Jaden Smith.
 String.prototype.toJadenCase = function () {
   return this.charAt(0).toUpperCase() + this.slice(1).replace(/ [a-z]/g, (substr) => substr.toUpperCase());
 };
