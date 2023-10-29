5aac44afdfab4d7edf66a2a6ba4144dfc64f067f
alisev <45853990+alisev@users.noreply.github.com>
2023-10-29 13:57:06 +0200

    Update isPangram.js

--- isPangram.js
+++ isPangram.js
@@ -1,4 +1,5 @@
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+// time complexity: O(n+26) = O(n)
 function isPangram(string){
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const lowStr = string.toLowerCase();
