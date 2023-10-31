714861283473782d00b550c0c92db9e9e4ad9fb5
alisev <45853990+alisev@users.noreply.github.com>
2023-10-31 20:28:41 +0200

    Create isLeapYear.js

---
+++ 7kyu/isLeapYear.js
@@ -0,0 +1,14 @@
+//mine
+function isLeapYear(year) {
+  if (year % 400 === 0) return true;
+  else if (year % 100 === 0) return false;
+  else if (year % 4 === 0) return true;
+  return false;
+}
+
+//found
+const moment = require('moment');
+
+function isLeapYear(year) {
+  return moment([year]).isLeapYear()
+}
