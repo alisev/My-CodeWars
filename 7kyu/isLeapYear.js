f1f423453d81c53f35f2652f5ea41b49360a0afa
alisev <45853990+alisev@users.noreply.github.com>
2023-11-01 15:54:02 +0200

    Update isLeapYear.js

--- 7kyu/isLeapYear.js
+++ 7kyu/isLeapYear.js
@@ -1,9 +1,16 @@
-//mine
+// Check if given year is a leap year.
+
+//version 1
 function isLeapYear(year) {
   if (year % 400 === 0) return true;
   else if (year % 100 === 0) return false;
   else if (year % 4 === 0) return true;
   return false;
+}
+
+// version 2 - optimized, return line contains only the true cases
+function isLeapYear(year) {
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
 }
 
 //found
