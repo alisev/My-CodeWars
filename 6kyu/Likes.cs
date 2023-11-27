4cf6739327c6061054bdd9a0e06858c80ea673cd
Alise <45853990+alisev@users.noreply.github.com>
2023-11-27 12:17:00 +0200

    Create Likes.cs

---
+++ 6kyu/Likes.cs
@@ -0,0 +1,24 @@
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+//
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+using System;
+
+public static class Kata
+{
+  public static string Likes(string[] name)
+  {   
+    int names = name.Length;   
+    switch (names) {
+        case 0: return "no one likes this";
+        case 1: return $"{name[0]} likes this";
+        case 2: return $"{name[0]} and {name[1]} like this";
+        case 3: return $"{name[0]}, {name[1]} and {name[2]} like this";
+        default: return $"{name[0]}, {name[1]} and {names - 2} others like this";
+    }
+  }
+}
