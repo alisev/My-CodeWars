3854be06af8bc9a2248c44b8ed1d7f22cb13c31b
Alise <45853990+alisev@users.noreply.github.com>
2023-12-02 14:03:37 +0200

    Create up_and_down.sql

---
+++ 7kyu/up_and_down.sql
@@ -0,0 +1,9 @@
+-- Your job is to return table with similar structure and headings,
+-- where if the sum of a column is odd, the column shows the minimum value for that column,
+-- and when the sum is even, it shows the max value. You must use a case statement.
+
+-- version 1
+SELECT
+  CASE WHEN SUM(number1) % 2 = 0 THEN MAX(number1) ELSE MIN(number1) END AS number1,
+  CASE WHEN SUM(number2) % 2 = 0 THEN MAX(number2) ELSE MIN(number2) END AS number2
+FROM numbers
